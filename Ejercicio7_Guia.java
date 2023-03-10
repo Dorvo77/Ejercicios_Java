f3cc764c15b09966192bfb938cfb8fcfb1bb0b71
Nicolas Barbetta <nbarbetta@gmail.com>
2023-03-10 13:43:16 -0300

    segundo commit

---
+++ Ejercicio7_Guia.java
@@ -0,0 +1,29 @@
+/* Considera que estás desarrollando una web para una empresa que fabrica motores 
+(suponemos que se trata del tipo de motor de una bomba para mover fluidos).
+ Definir una variable tipoMotor y permitir
+ que el usuario ingrese un valor entre 1 y 4. El programa debe mostrar lo siguiente:
+ */
+ 
+ class Ejercicio7_Guia{
+	 public static void main (Syring[] args){
+		Scanner leer = new Scanner(System.in);
+        System.out.println("INgrese tipo de motor. 1-4 ");
+		int tipoMotor = leer.nextInt();
+		switch (tipoMotor){
+			case 1:
+			System.out.println("La bomba es una bomba de agua");
+			break;
+			case 2:
+			System.out.println("La bomba es una bomba de gasolina");
+			break;
+			case 3:
+			System.out.println("La bomba es una bomba de hormigon");
+			break;
+			case 4:
+			System.out.println("La bomba es una bomba de alimenticia");
+			break;
+			default:
+			System.out.println("No existe valor valido...");
+		}
+	 }
+ }
